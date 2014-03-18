f6525bee182b2157a6ab02137b365eee71687603
Fernando Lira <fernando.lira@gmail.com>
2014-03-17 23:00:23 -0300

    Atualização 17/03
    - Correções de html
    - Correções de CSS
    - Começo do script para itens de acessibilidade

---
+++ tcu/js/functions.js
@@ -0,0 +1,83 @@
+$(init( ));
+
+function init ( ) {
+	addClickFunctions( );
+	addResizeFunction( );
+}
+
+function addClickFunctions( ) {
+	//bloqueia o link original
+	$('#fontBtn').click(function(e){e.preventDefault( );});
+	
+	$('#menuBtn').click(function(){toggleMainMenu ( );});
+	$('#fontBtn').click(function(){toggleFontAdjust( );});
+}
+
+function toggleMainMenu ( ) {
+	if (!$('#mainMenu nav').hasClass('active')) {
+		showMenu( );
+	}	else {
+		hideMenu( );
+	}
+}
+
+function showMenu( ) {
+	$('#mainMenu nav').addClass('active');
+	$('body').css({
+		'position':'absolute',
+		'width':$('body').width(),
+		'overflow-x':'hidden'
+	});
+	$('body').finish( ).animate({
+		'left': $('#mainMenu nav').width(),
+	});
+	$('#mainMenu nav').finish( ).animate({
+		'left': 0
+	});
+}
+function hideMenu( ) {
+	$('#mainMenu nav').removeClass('active');
+	$('body').finish( ).animate({
+		'left': 0
+	},function() {
+		clearInlines(['body']);
+		
+	});
+	$('#mainMenu nav').finish( ).animate({
+		'left': - $('#mainMenu nav').width()
+	},function() {
+		clearInlines(['#mainMenu nav']);
+	});
+}
+
+function toggleFontAdjust( ) {
+	if (!$('#fontSize').hasClass('active')) {
+		activeAcessibilityPanel( );
+		showFontHelp( );
+	}	else {
+		deactiveAcessibilityPanel( );
+		hideFontHelp( );
+	}
+}
+
+function showFontHelp( ) {
+	
+}
+
+function hideFontHelp( ) {
+
+}
+
+function addResizeFunction( ) {
+	//reseta o menu principal se ele estiver expandido
+	$(window).resize(function(){
+		clearInlines(['body','#mainMenu nav']);
+		$('#mainMenu nav').removeClass('active');
+	});
+}
+
+function clearInlines(elements) {
+	for (var i=0;i<elements.length;i++) {
+		$(elements[i]).removeAttr('style');
+	}
+}
